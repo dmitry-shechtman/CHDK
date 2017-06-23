472019177f4753dca3a0e7735831f8ded7c703b6
Dmitry Shechtman <damnian@gmail.com>
2017-06-23 19:02:53 +0300

    Duplicate meta_modules_get() in gen_meta_modules

--- tools/gen_meta/gen_meta_modules.c
+++ tools/gen_meta/gen_meta_modules.c
@@ -14,6 +14,10 @@
  * GNU General Public License for more details.
  */
 
+#include <dirent.h>
+
+#include "../include/versions.h"
+#include "flt.h"
 #include "meta_modules.h"
 #include "cli.h"
 
@@ -66,9 +70,81 @@ static int parse_args(int argc, char const* argv[], char const** category, char 
 	return 0;
 }
 
+#define MODULE_TYPE_COUNT 5
+
+static char* module_types[MODULE_TYPE_COUNT] = { 0, "extension", "game", "tool", "script" };
+
+static int gen_meta_modules_get(meta_modules_t* modules, const char* modPath, const char* created, const char* changeset) {
+    DIR *d;
+    struct dirent *de;
+    static ModuleInfo mi;
+    static char modName[33];
+    static const char hash_name[] = "sha256";
+    static unsigned char sha256[SHA256_HASH_SIZE];
+    meta_modules_item_t* item;
+    int i = 0;
+    if (!modules || !modPath) {
+        return 0;
+    }
+    if (!(d = opendir(modPath))) {
+        return 0;
+    }
+    modules->count = 0;
+    while ((de = readdir(d))) {
+        if (de->d_name[0] != 0xE5 && strcmp(de->d_name, ".") && strcmp(de->d_name, "..") && strcmp(de->d_name, "CFG")) {
+            modules->count++;
+        }
+    }
+    closedir(d);
+    if (!(modules->modules = malloc(modules->count * sizeof(meta_modules_item_t)))) {
+        return 0;
+    }
+    if (!(d = opendir(modPath))) {
+        free(modules->modules);
+        modules->modules = 0;
+        modules->count = 0;
+        return 0;
+    }
+    while ((de = readdir(d))) {
+        if (de->d_name[0] != 0xE5 && strcmp(de->d_name, ".") && strcmp(de->d_name, "..") && strcmp(de->d_name, "CFG")) {
+            item = &modules->modules[i++];
+            get_module_info_and_hash(modPath, de->d_name, &mi, modName, sizeof(modName), sha256);
+            if ((item->key = malloc(strlen(de->d_name) + 1))) {
+                meta_strtolower(item->key, de->d_name);
+            }
+            item->value.typeId = mi.moduleType & MTYPE_MASK;
+            if (item->value.typeId < MODULE_TYPE_COUNT)
+                item->value.type = module_types[item->value.typeId];
+            else
+                item->value.type = 0;
+            item->value.titleId = mi.moduleName < 0 ? -mi.moduleName : 0;
+            if (mi.moduleName < 0) {
+                item->value.title = 0;
+            }
+            else if ((item->value.title = malloc(strlen(modName) + 1))) {
+                strcpy(item->value.title, modName);
+            }
+            item->value.imageId = mi.symbol;
+            item->value.created = created;
+            item->value.changeset = changeset;
+            item->value.hash.name = hash_name;
+            if ((item->value.hash.items = malloc(sizeof(meta_hash_item_t)))) {
+                if ((item->value.hash.items[0].filename = malloc(strlen(de->d_name) + 14))) {
+                    sprintf(item->value.hash.items[0].filename, "CHDK\\MODULES\\%s", item->key);
+                }
+                memcpy(item->value.hash.items[0].hash, sha256, SHA256_HASH_SIZE);
+                item->value.hash.items[0].size = SHA256_HASH_SIZE;
+                item->value.hash.count = 1;
+            }
+        }
+    }
+    closedir(d);
+    return -1;
+}
+
 int main(int argc, char const* argv[])
 {
-    meta_modules_t modules = { "1.0" };
+    meta_modules_t modules;
     const char* category = NULL;
     const char* product = NULL;
 	const char* modpath = NULL;
@@ -117,10 +193,11 @@ int main(int argc, char const* argv[])
 		return version();
 	}
 
+    modules.version = "1.0";
     modules.category.name = category;
     modules.product.name = product;
 
-    if (!meta_modules_get(&modules, modpath, created, changeset))
+    if (!gen_meta_modules_get(&modules, modpath, created, changeset))
     {
         fprintf(stderr, "Error scanning %s\n", modpath);
         return -1;
@@ -143,6 +220,8 @@ int main(int argc, char const* argv[])
     meta_modules_write(&modules, json);
 	json_close(json);
 	fclose(fout);
+
+    meta_modules_free(&modules);
 
 	return result;
 }
